3011bb21df6261dc11ec15dff84e8f86d577ffd9
gabrieljakubek <gabrieljakubek@gmail.com>
2019-09-15 23:22:39 -0300

    Se avanzo con el ejercicio de crear un formulario de anuncios

---
+++ Practica/Ejercicios/Ejercicio 01/script.js
@@ -0,0 +1,13 @@
+var anuncio;
+var anuncios = [];
+
+function cargarAnuncio(){
+    anuncio.titulo = a;
+    anuncio.descripcion = b;
+    anuncio.operacion=c;
+    anuncio.precio=d;
+    anuncio.dormitorios =e;
+    anuncio.baños = f;
+    anuncio.garages =g;
+    anuncios += anuncio;
+}
