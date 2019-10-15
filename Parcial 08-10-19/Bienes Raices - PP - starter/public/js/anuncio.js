2b1b904f563630dd8fff1a34a0898e90e2893411
gabrieljakubek <gabrieljakubek@gmail.com>
2019-10-14 21:14:55 -0300

    Se realizo el parcial

---
+++ Parcial 08-10-19/Bienes Raices - PP - starter/public/js/anuncio.js
@@ -0,0 +1,10 @@
+function Anuncio(id, titulo, transaccion, descripcion, precio, num_wc, num_estacionamiento, num_dormitorio) {
+    this.id = id;
+    this.titulo = titulo;
+    this.transaccion = transaccion;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.num_dormitorio = num_dormitorio;
+    this.num_wc = num_wc;
+    this.num_estacionamiento = num_estacionamiento;
+}
